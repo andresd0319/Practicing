334d32f376daca03bb1e08720f6c65c554185326
Andrés Gutiérrez <114630183+andresd0319@users.noreply.github.com>
2023-03-03 22:23:04 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let btnOpenMenu = document.querySelector("#open-menu");
+let btnCloseMenu = document.querySelector("#close-menu");
+let menu = document.querySelector("#mobile-menu");
+
+btnOpenMenu.addEventListener("click", () =>{
+    menu.classList.remove("disabled");
+})
